e4285716c12d7b9c4cc0ceb551e6a1a14dec8e25
Andre Arriola <84458170+Jjokerrrr@users.noreply.github.com>
2021-12-14 23:37:51 +0800

    Initial commit

---
+++ src/main/java/com/csb/samplefullstack/controller/Greet.java
@@ -0,0 +1,18 @@
+package com.csb.samplefullstack.controller;
+
+import com.csb.samplefullstack.computations.Computation;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/greet")
+@CrossOrigin("*") //allow all connections
+public class Greet {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return  "hello" + Computation.sumOfTwoNums();
+    }
+
+}
